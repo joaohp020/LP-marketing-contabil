0b18bee7c94ce78dbc9a6ca355cde9bdca028fb9
João Henrique <jh36750@gmail.com>
2024-12-03 10:51:15 -0300

    ajuste no layout responsivo 1440px

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,13 +7,3 @@ document.querySelectorAll('.btnPrincipal').forEach(button => {
         }
     });
 });
-
-// document.querySelectorAll('.btnSecundario').forEach(button => {
-//     button.addEventListener('click', (event) => {
-//         event.preventDefault(); // Evita o comportamento padrão do link
-//         const contatoSection = document.querySelector('.cards'); // Alterar para o ID ou classe da seção de contato
-//         if (contatoSection) {
-//             contatoSection.scrollIntoView({ behavior: 'smooth' });
-//         }
-//     });
-// });
