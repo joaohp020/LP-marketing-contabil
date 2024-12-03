8a5201eae60ceb08145d6e01bb72825a21e2832a
João Henrique <jh36750@gmail.com>
2024-12-03 11:04:47 -0300

    ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.btnPrincipal').forEach(button => {
     button.addEventListener('click', (event) => {
-        event.preventDefault(); // Evita o comportamento padrão do link
+        event.preventDefault();
         const contatoSection = document.querySelector('.formulario'); // Alterar para o ID ou classe da seção de contato
         if (contatoSection) {
             contatoSection.scrollIntoView({ behavior: 'smooth' });
